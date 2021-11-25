cc2fd65c6cbf4e4b4324564d1d5941948b3b0879
Tymon-Jasiukiewicz2006 <73474137+Tymon-Jasiukiewicz2006@users.noreply.github.com>
2021-11-25 22:12:25 +0100

    Create conditional statements.cpp

---
+++ conditional statements.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    if(a>9) {
+        cout << "Greater than 9";
+    }
+    else if (a == 1) {
+        cout << "one";
+    }
+    else if (a == 2) {
+        cout << "two";
+    }
+    else if (a == 3) {
+        cout << "three";
+    }
+    else if (a == 4) {
+        cout << "four";
+    }
+    else if (a == 5) {
+        cout << "five";
+    }
+    else if (a == 6) {
+        cout << "six";
+    }
+    else if (a == 7) {
+        cout << "seven";
+    }
+    else if (a == 8) {
+        cout << "eight";
+    }
+    else {
+        cout << "nine";
+    }
+    return 0;
+}
