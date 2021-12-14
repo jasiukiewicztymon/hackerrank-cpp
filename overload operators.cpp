d1f73de3976af319f6818a8f8096094b5b14f5be
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2021-12-14 21:59:15 +0100

    Create overload operators.cpp

---
+++ overload operators.cpp
@@ -0,0 +1,59 @@
+/Operator Overloading
+
+#include<iostream>
+
+using namespace std;
+
+class Complex
+{
+public:
+    int a,b;
+    void input(string s)
+    {
+        int v1=0;
+        int i=0;
+        while(s[i]!='+')
+        {
+            v1=v1*10+s[i]-'0';
+            i++;
+        }
+        while(s[i]==' ' || s[i]=='+'||s[i]=='i')
+        {
+            i++;
+        }
+        int v2=0;
+        while(i<s.length())
+        {
+            v2=v2*10+s[i]-'0';
+            i++;
+        }
+        a=v1;
+        b=v2;
+    }
+};
+
+//here is the code
+Complex operator+ (Complex const &x, Complex const &y) {
+    Complex res;
+    res.a = x.a + y.a;
+    res.b = x.b + y.b;
+    return res;
+}
+
+ostream& operator<<(ostream& os, const Complex &x)
+{
+    os << x.a << "+i" << x.b << endl;
+    return os;
+}
+
+int main()
+{
+    Complex x,y;
+    string s1,s2;
+    cin>>s1;
+    cin>>s2;
+    x.input(s1);
+    y.input(s2);
+    Complex z=x+y;
+    cout<<z<<endl;
+}
