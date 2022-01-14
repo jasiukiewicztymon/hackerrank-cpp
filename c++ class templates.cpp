973a3ada402e05b060ee72e164175d884e4352d1
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-01-14 22:53:35 +0100

    Create c++ class templates.cpp

---
+++ c++ class templates.cpp
@@ -0,0 +1,51 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+template <class T> class AddElements {
+    private:
+        T e;
+    public:
+        AddElements(T input) { e = input; }
+        T add(T input) { return e + input; }        
+};
+
+template <> class AddElements <string> {
+    private:
+        string e;
+    public:
+        AddElements(string input) { e = input; }
+        string concatenate(string input) { return e + input; }  
+};
+
+int main () {
+  int n,i;
+  cin >> n;
+  for(i=0;i<n;i++) {
+    string type;
+    cin >> type;
+    if(type=="float") {
+        double element1,element2;
+        cin >> element1 >> element2;
+        AddElements<double> myfloat (element1);
+        cout << myfloat.add(element2) << endl;
+    }
+    else if(type == "int") {
+        int element1, element2;
+        cin >> element1 >> element2;
+        AddElements<int> myint (element1);
+        cout << myint.add(element2) << endl;
+    }
+    else if(type == "string") {
+        string element1, element2;
+        cin >> element1 >> element2;
+        AddElements<string> mystring (element1);
+        cout << mystring.concatenate(element2) << endl;
+    }
+  }
+  return 0;
+}
