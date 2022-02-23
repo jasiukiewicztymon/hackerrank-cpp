6d3532fdf549ac0efaf2dae701f873db68fe4dcd
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-02-23 18:44:28 +0100

    Create Accessing Inherited Functions.cpp

---
+++ Accessing Inherited Functions.cpp
@@ -0,0 +1,118 @@
+#include<iostream>
+
+using namespace std;
+
+class A
+{
+    public:
+        A(){
+            callA = 0;
+        }
+    private:
+        int callA;
+        void inc(){
+            callA++;
+        }
+
+    protected:
+        void func(int & a)
+        {
+            a = a * 2;
+            inc();
+        }
+    public:
+        int getA(){
+            return callA;
+        }
+};
+
+class B
+{
+    public:
+        B(){
+            callB = 0;
+        }
+    private:
+        int callB;
+        void inc(){
+            callB++;
+        }
+    protected:
+        void func(int & a)
+        {
+            a = a * 3;
+            inc();
+        }
+    public:
+        int getB(){
+            return callB;
+        }
+};
+
+class C
+{
+    public:
+        C(){
+            callC = 0;
+        }
+    private:
+        int callC;
+        void inc(){
+            callC++;
+        }
+    protected:
+        void func(int & a)
+        {
+            a = a * 5;
+            inc();
+        }
+    public:
+        int getC(){
+            return callC;
+        }
+};
+class D  : protected A, B, C
+{
+
+	int val;
+	public:
+		D() { val = 1; }
+		void update_val(int new_val)
+		{
+            while (new_val % 2 == 0)
+            {
+                new_val = new_val/2;
+                A::func(val);
+            }
+            while (new_val % 3 == 0)
+            {
+                new_val = new_val/3;
+                B::func(val);
+            }
+            while (new_val % 5 == 0)
+            {
+                new_val = new_val/5;
+                C::func(val);
+            }
+
+        }
+		void check(int);
+};
+
+
+
+void D::check(int new_val)
+{
+    update_val(new_val);
+    cout << "Value = " << val << endl << "A's func called " << getA() << " times " << endl << "B's func called " << getB() << " times" << endl << "C's func called " << getC() << " times" << endl;
+}
+
+
+int main()
+{
+    D d;
+    int new_val;
+    cin >> new_val;
+    d.check(new_val);
+
+}
