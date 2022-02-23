c5f0e057626f53f14898983df7a95305e585c259
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-02-23 16:08:49 +0100

    Create Virtual Functions.cpp

---
+++ Virtual Functions.cpp
@@ -0,0 +1,85 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Person {
+    protected:
+        string name;
+        int age;     
+    public:
+        virtual void getdata() {
+            cin >> this->name >> this->age; 
+        }
+        virtual void putdata() {
+            cout << name << " " << age << endl;
+        }  
+};
+
+class Professor : public Person {
+    private:
+        int publications, cur_id;
+    public:
+        static int id;
+        Professor() {
+            this->cur_id = ++id;
+        }
+        void getdata() {
+            cin >> this->name >> this->age >> publications; 
+        }
+        void putdata() {
+            cout << name << " " << age << " " << publications << " " << cur_id << endl;
+        }
+};
+
+class Student : public Person {
+    private:
+        int marks[6], cur_id, total = 0;
+    public:
+        static int id;
+        Student() {
+            
+            this->cur_id = ++id;
+        }
+        void getdata() {
+            cin >> this->name >> this->age;
+            for (int i = 0; i < 6; i++) {
+                cin >> marks[i];
+                total += marks[i];
+            } 
+        }
+        void putdata() {
+            cout << name << " " << age << " " << total << " " << cur_id << endl;
+        }
+};
+int Professor::id = 0;
+int Student::id = 0;
+
+int main(){
+
+    int n, val;
+    cin>>n; //The number of objects that is going to be created.
+    Person *per[n];
+
+    for(int i = 0;i < n;i++){
+
+        cin>>val;
+        if(val == 1){
+            // If val is 1 current object is of type Professor
+            per[i] = new Professor;
+
+        }
+        else per[i] = new Student; // Else the current object is of type Student
+
+        per[i]->getdata(); // Get the data from the user.
+
+    }
+
+    for(int i=0;i<n;i++)
+        per[i]->putdata(); // Print the required output for each object.
+
+    return 0;
+
+}
